2bd716e53eaea564119f61c0d79dae9e1d7bba0f
CornK <kld9xxhy@gmail.com>
2020-10-06 23:40:16 +0700

    Initial commit

---
+++ app/src/main/java/com/cornk/calculator/MainActivity.java
@@ -0,0 +1,14 @@
+package com.cornk.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_calculator);
+    }
+}
